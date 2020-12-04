08b54c5ab1ab7e452f5a9435191c00aea304a1f3
Tay Choon Kiat <choonkiattay@gmail.com>
2020-12-04 18:33:22 +0800

    Add files via upload

--- latlong_km_bak.py
+++ latlong_km_bak.py
@@ -4,16 +4,6 @@ import pandas as pd
 import time
 import numpy as np
 from multiprocessing import Pool
-import argparse
-
-def init_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input', type=str, help='input file name, excel or csv')
-    parser.add_argument('--output', type=str, help='output file name, excel or csv')
-    parser.add_argument('--within_radius', type=str, help='unit in km')
-    parser.print_help()
-
-    return parser.parse_args()
 
 def haversine_dist(ori, dest):
     lat1, lon1 = ori
@@ -30,14 +20,14 @@ def haversine_dist(ori, dest):
 
     return dist
 
-def site_within(group, site_latlong, radius):
+def site_within(group, site_latlong):
     other_sites = latlong_df['site_latlong'].loc[latlong_df['group'] != group].tolist()
     sites = site_latlong[0]
     latlong = site_latlong[1]
     distance = [haversine_dist(latlong, x[1]) for x in other_sites]
     # distance_within = [x for x in distance]# if x< 0.3]
     site_km = list(zip([x[0] for x in other_sites], distance))
-    site_km = [x for x in site_km if x[1]< radius]
+    site_km = [x for x in site_km if x[1]< 2.0]
     site_km = sorted(site_km, key=lambda tup: tup[1])
     return site_km
 
@@ -50,18 +40,15 @@ def parallelize_dataframe(df, func, n_cores=4):
     return df
 
 def nearest(latlong_df):
-    latlong_df['nearest'] = latlong_df.apply(lambda x: site_within(x['group'], x['site_latlong'], x['radius_limit']), axis=1)
+    latlong_df['nearest'] = latlong_df.apply(lambda x: site_within(x['group'], x['site_latlong']), axis=1)
     return latlong_df
 
 if __name__ == "__main__":
 
-    args = init_args()
-
     start_time = time.time()
-    latlong_df = pd.read_excel(args.input, header=1, index_col=0)
+    latlong_df = pd.read_excel('input_file/latlong_data.xlsx', header=1, index_col=0)
     latlong_df['site_latlong'] = list(zip(latlong_df['site'],list(zip(latlong_df['lat'],latlong_df['long']))))
-    latlong_df['radius_limit'] = float(args.within_radius)
     final_df = parallelize_dataframe(latlong_df, nearest)
-    final_df.to_excel(args.output)
+    final_df.to_excel('output/result.xlsx')
     
     print("--- Processed in {} seconds ---".format((time.time() - start_time)))
